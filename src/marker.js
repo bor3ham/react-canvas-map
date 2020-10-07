d9038b45be08b245203250fe22eb22e97610e64b
Michael Boreham <bor3ham@gmail.com>
2020-10-08 10:42:33 +1100

    Function-based marker

--- src/marker.js
+++ src/marker.js
@@ -1,36 +1,34 @@
-import React from 'react'
 import PropTypes from 'prop-types'
 
 import { coordsShape } from './shapes.js'
 
 /* eslint-disable react/no-unused-prop-types */
 
-export default class Marker extends React.Component {
-  static propTypes = {
-    markerKey: PropTypes.string,
-    coords: coordsShape.isRequired,
-
-    image: PropTypes.object,
-    inCircle: PropTypes.bool,
-    circleColour: PropTypes.string,
-    size: PropTypes.number,
-    scaleWithZoom: PropTypes.bool,
-
-    dragZoneScale: PropTypes.number,
-    onDragTick: PropTypes.func,
-    onDragEnd: PropTypes.func,
-    onDragCancel: PropTypes.func,
-  }
-
-  static defaultProps = {
-    inCircle: false,
-    circleColour: '#337ab7',
-    size: 100,
-    scaleWithZoom: true,
-    dragZoneScale: 1,
-  }
-
-  render() {
-    return null
-  }
+function Marker(props) {
+  return null
 }
+Marker.propTypes = {
+  markerKey: PropTypes.string,
+  coords: coordsShape.isRequired,
+
+  image: PropTypes.object,
+  inCircle: PropTypes.bool,
+  circleColour: PropTypes.string,
+  size: PropTypes.number,
+  scaleWithZoom: PropTypes.bool,
+
+  dragZoneScale: PropTypes.number,
+  onDragTick: PropTypes.func,
+  onDragEnd: PropTypes.func,
+  onDragCancel: PropTypes.func,
+}
+
+Marker.defaultProps = {
+  inCircle: false,
+  circleColour: '#337ab7',
+  size: 100,
+  scaleWithZoom: true,
+  dragZoneScale: 1,
+}
+
+export default Marker
